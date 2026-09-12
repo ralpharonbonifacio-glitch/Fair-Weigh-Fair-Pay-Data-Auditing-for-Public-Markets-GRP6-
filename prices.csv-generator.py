d9d3aae1675db5b8b3bf61bde950b9e4990ec082
Josef Nepomuceno <josefjayden.nepomuceno@ust.edu.ph>
2026-09-12 20:51:32 +0800

    feat: complete prices.csv generator logic with dynamic subset selection and bounded pricing

--- prices.csv-generator.py
+++ prices.csv-generator.py
@@ -28,5 +28,30 @@ commodities_chosen = np.random.choice(commodity_list, size=commodity_amount, rep
 #Assign the chosen commodities to your rows
 commodity = np.random.choice(commodities_chosen, size=days)
 
+#created minmax bound for avg_price_per_kg so each item price is different and actually makes sense for a synthetic
 
-avg_price_per_kg = None
+commodity_price_minmax = {
+    'Rice': (45.0, 60.0),
+    'Chicken': (180.0, 220.0),
+    'Pork': (300.0, 380.0),
+    'Beef': (350.0, 450.0),
+    'Bangus': (160.0, 220.0),
+    'Tomatoes': (60.0, 120.0),
+    'Onions': (80.0, 150.0),
+    'Garlic': (100.0, 180.0),
+    'Eggplant': (60.0, 90.0),
+    'Cabbage': (80.0, 140.0),
+    'Carrots': (90.0, 150.0),
+    'Potatoes': (90.0, 140.0),
+    'Tilapia': (120.0, 160.0),
+    'Ginger': (100.0, 160.0)
+}
+
+#pull bounds, commodity_price_minmax(dictionary)[x][0] to call min value, [x][1] to call for max, 
+#for x in commodity so it calls for each name in list
+minprice = [commodity_price_minmax[x][0] for x in commodity]
+maxprice = [commodity_price_minmax[x][1] for x in commodity]
+
+#calculate average price per kg from each unique commodity
+avg_price_per_kg = np.round(np.random.uniform(minprice, maxprice), 2)
+
