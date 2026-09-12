23c2d372ee23222fd7b3861c491a0b12a08102d4
Josef Nepomuceno <josefjayden.nepomuceno@ust.edu.ph>
2026-09-12 10:16:42 +0800

    wip: add initial setup and variables for inspections script

--- creation-inspections.csv-dataframe.py
+++ creation-inspections.csv-dataframe.py
@@ -1,16 +1,22 @@
+import numpy as np
+import pandas as pd
+
+np.random.seed(1)
+
 'For generating inspections.csv'
 
 # Inspection dates, starts at 2026-01-01, 16 weeks, every Thursday
-date = None
+date = pd.date_range(start='2026-01-01', periods=16, freq='W-THU')
 
-market_id = None #Market ID -> 1-5 Markets
-stall_id = None #Stall ID -> 30-50 Stalls
-scale_id = None #Scale ID -> 1-2 Scales per Stall
-inspector_id = None #Inspector ID - 1-5 Inspectors
+#IDs
+market_id = np.random.randint(1, 6, size=16) #Market ID - 1-5 Markets
+stall_id = np.random.randint(30, 51,size=16) #Stall ID - 30-50 Stalls
+scale_id = np.random.randint(1, 2, size=16) #Scale ID - 1 or 2 Scales per Stall
+inspector_id = np.random.randint(1, 6, size=16) #Inspector ID - 1-5 Inspectors
 
 #certifcation date logic
 is_certified = None #Check if inspector is certified
-last_cert_date = None #latest cert date for inspector
+last_certs_date = None #latest cert date for inspector
 is_certified = None #Check if inspector last cert date is within 365 days, else expired
 
 #Weight calculation
