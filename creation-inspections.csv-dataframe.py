0223172e01438a81d9cd56552802cb0a254c8414
Josef Nepomuceno <josefjayden.nepomuceno@ust.edu.ph>
2026-09-12 10:48:47 +0800

    refactor: randomize inspection period length between 90 and 120 days

--- creation-inspections.csv-dataframe.py
+++ creation-inspections.csv-dataframe.py
@@ -5,17 +5,20 @@ np.random.seed(1)
 
 'For generating inspections.csv'
 
+#random amount of days chosen between 90-120
+days = np.random.randint(90, 121)
+
 # Inspection dates, starts at 2026-01-01, 16 weeks, every Thursday
-date = pd.date_range(start='2026-01-01', periods=16, freq='W-THU')
+date = pd.date_range(start='2026-01-01', periods=days, freq='W-THU')
 
 #IDs
-market_id = np.random.randint(1, 6, size=16) #Market ID - 1-5 Markets
-stall_id = np.random.randint(30, 51,size=16) #Stall ID - 30-50 Stalls
-scale_id = np.random.randint(1, 2, size=16) #Scale ID - 1 or 2 Scales per Stall
-inspector_id = np.random.randint(1, 6, size=16) #Inspector ID - 1-5 Inspectors
+market_id = np.random.randint(1, 6, size=days) #Market ID - 1-5 Markets
+stall_id = np.random.randint(30, 51,size=days) #Stall ID - 30-50 Stalls
+scale_id = np.random.randint(1, 2, size=days) #Scale ID - 1 or 2 Scales per Stall
+inspector_id = np.random.randint(1, 6, size=days) #Inspector ID - 1-5 Inspectors
 
 #certifcation date logic
-randdays = np.random.randint(30, 501, size=16) #random amount of days for synthetic data generation
+randdays = np.random.randint(30, 501, size=days) #random amount of days for synthetic data generation
 last_cert_date = (date - pd.to_timedelta(randdays, unit="D")).strftime("%Y-%m-%d")
 is_certified = np.where(randdays <= 365, "Y", "N") #Check if inspector last cert date is within 365 days, else expired
 
@@ -26,10 +29,10 @@ date_as_data = date.strftime("%Y-%m-%d")
 
 #weight calibration
 #Nominal Weight tests - 100g, 250g, 500g, 1000g
-nominal_weight_g = np.random.choice([100, 250, 500, 1000], size=16)
+nominal_weight_g = np.random.choice([100, 250, 500, 1000], size=days)
 
 #Error percentage, Minimum and maximum of at least 70% to simulate possible fraud
-error_perc = np.random.uniform(-0.75, 0.75, size=16)
+error_perc = np.random.uniform(-0.75, 0.75, size=days)
 
 #Reading Weight from using scale, difference varies based on error percentage
 reading_g = np.round(nominal_weight_g * (1 + error_perc), 2)
